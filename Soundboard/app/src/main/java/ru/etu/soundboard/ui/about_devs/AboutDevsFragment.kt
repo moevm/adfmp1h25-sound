4c43956f447d14c06efcc85750e29c7547949c8f
homin12 <vova.chernuxa@mail.ru>
2025-03-04 23:37:03 +0300

    totally insane devs page

---
+++ Soundboard/app/src/main/java/ru/etu/soundboard/ui/about_devs/AboutDevsFragment.kt
@@ -0,0 +1,40 @@
+package ru.etu.soundboard.ui.about_devs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.etu.soundboard.databinding.FragmentAboutDevsBinding
+
+class AboutDevsFragment : Fragment() {
+
+    private var _binding: FragmentAboutDevsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(AboutDevsViewModel::class.java)
+
+        _binding = FragmentAboutDevsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
