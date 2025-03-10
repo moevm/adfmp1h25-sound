12b54e1ab675944230893a7d59952171df96ae63
Popug-s-catcher <ievlevegor03.taxix@gmail.com>
2025-03-10 22:50:00 +0300

    beauty update

--- Soundboard/app/src/main/java/ru/etu/soundboard/AboutDevs.kt
+++ Soundboard/app/src/main/java/ru/etu/soundboard/AboutDevs.kt
@@ -1,14 +1,11 @@
 package ru.etu.soundboard
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AboutDevs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_about_devs)
+        setContentView(R.layout.about_devs)
     }
 }
