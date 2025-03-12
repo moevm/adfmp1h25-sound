145d79b6d409321271f2dfe50ce1d72d645fedb3
KirillSamokhin <samohink575@gmail.com>
2025-03-12 19:18:40 +0300

    add player routs. hide status bar

--- Soundboard/app/src/main/java/ru/etu/soundboard/MainActivity.kt
+++ Soundboard/app/src/main/java/ru/etu/soundboard/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.etu.soundboard
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -39,6 +40,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MyTracks::class.java)
             startActivity(intent)
         }
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
         /*val navView: NavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
